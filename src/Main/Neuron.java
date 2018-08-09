5ffa8cf70ce451ec650103540673df1ea8ff3acd
Nitesh Reddy <nitesh@pop-os.localdomain>
2018-08-09 17:18:31 -0400

    Updating outputs

--- src/Main/Neuron.java
+++ src/Main/Neuron.java
@@ -5,4 +5,29 @@ public class Neuron {
 	private float[] weights;
 	private float output;
 	
+	public Neuron(double[] inputs, float[] weights) {
+		super();
+		this.inputs = inputs;
+		this.weights = weights;
+	}
+	public double[] getInputs() {
+		return inputs;
+	}
+	public void setInputs(double[] inputs) {
+		this.inputs = inputs;
+	}
+	public float[] getWeights() {
+		return weights;
+	}
+	public void setWeights(float[] weights) {
+		this.weights = weights;
+	}
+	public float getOutput() {
+		return output;
+	}
+	public void setOutput(float output) {
+		this.output = output;
+	}
+	
+	
 }
