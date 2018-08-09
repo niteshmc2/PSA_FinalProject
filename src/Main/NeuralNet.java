31405d223741e39c02e2a4413b8b7d6fbffba054
Nitesh Reddy <nitesh@pop-os.localdomain>
2018-08-09 17:42:25 -0400

    Clean up

--- src/Main/NeuralNet.java
+++ src/Main/NeuralNet.java
@@ -3,12 +3,9 @@ package Main;
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.io.InputStream;
 import java.util.Arrays;
 
 
@@ -40,52 +37,6 @@ public class NeuralNet {
 		return outputL;
 	}
 
-	public static Data readImage(String fileName) throws IOException{
-		Data data = new Data();
-		File file = new File(fileName);
-		//FileReader fr = new FileReader(f);
-		InputStream insputStream = new FileInputStream(file);
-		int length =(int) file.length();
-		System.out.println("Long length: "+file.length());
-		System.out.println("int length: "+length);
-		byte[] bytes = new byte[length];
-		insputStream.read(bytes);
-		insputStream.close();
-		int clas = bytes[0];
-		System.out.println(clas);
-		System.out.println(bytes[0]);
-        //File f = new File("MyFile.png");
-        
-        
-        for(int x = 0; x < 30730000; x += 3073) {
-        	int[] img = new int[1024];
-        		int cls = bytes[x];
-    	        byte[] reds = Arrays.copyOfRange(bytes, x+1, x+1025);
-    	        byte[] greens = Arrays.copyOfRange(bytes, x+1025, x+2049);
-    	        byte[] blues = Arrays.copyOfRange(bytes, x+2049, x+3073);
-    	        
-            	for(int z = 0; z < img.length; z++){
-            		int r = reds[z] & 0xFF;
-            		//System.out.println(r);
-                    int g = greens[z] & 0xFF; 
-                    int b = blues[z] & 0xFF;
-                    int col = (r << 16) | (g << 8) | b ;
-                    img[z] = col;
-        	}
-	      
-        	Images imgs = new Images(img, cls);
-        	data.addImage(imgs);
-        	
-        }
-        
-//        for(int i=20; i<30; i++){
-//        	data.getData().get(i).writeImg(String.valueOf(i));
-//        }
-       // System.out.println("Done");
-        
-        return data;
-		
-	}
 	
 	public double[][] computeDfuncOutput(double[][] input) {
 		for(int i=0; i<input.length; i++) {
